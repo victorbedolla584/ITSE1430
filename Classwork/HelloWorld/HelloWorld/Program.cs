0feea14432d704a42b4f955a55ce956289c810c0
victor.bedolla584 <victor.bedolla584@my.tccd.edu>
2018-01-22 21:40:40 -0600

    Hello world demo 2

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
+    // single line comment
     class Program
     {
+        /*
+         * as many lines as you want
+         */
         static void Main( string[] args )
         {
             Console.WriteLine("Hello World");
